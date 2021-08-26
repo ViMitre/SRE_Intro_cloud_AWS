5e486d4083dc3074101fda14ba8128725a01dd4b
ViMitre <vmitre@spartaglobal.com>
2021-08-26 15:37:22 +0000

    Thursday afternoon

---
+++ provision.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt-get install nginx -y
+sudo apt-get install git-core zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo apt-get install fonts-powerline -y
