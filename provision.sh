80f0cb391d551a3d511d54b45a843050a477d6fb
ViMitre <88156993+ViMitre@users.noreply.github.com>
2021-08-27 17:49:43 +0000

    Delete provision.sh

--- provision.sh
+++
@@ -1,8 +0,0 @@
-!#/bin/bash
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install nginx -y
-sudo apt-get install tmux -y
-# sudo apt-get install git-core zsh -y
-# sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -y)"
-# sudo apt-get install fonts-powerline -y
