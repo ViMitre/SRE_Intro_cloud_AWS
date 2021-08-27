8d534c9d5691d8dfe98d97e7616170557466e4fa
ViMitre <vmitre@spartaglobal.com>
2021-08-27 17:06:05 +0000

    Friday end

--- provision_db.sh
+++ provision_db.sh
@@ -8,3 +8,5 @@ sudo apt-get install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
 sudo ufw allow from 192.168.10.100/32 to any port 27017
+sudo rm /etc/mongod.conf
+sudo ln -s /home/vagrant/config_files/mongod.conf /etc/mongod.conf
