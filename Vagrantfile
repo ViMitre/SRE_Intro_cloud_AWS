2d2d522f890e3d5e92c9c9a925d8e7f789f96c80
ViMitre <vmitre@spartaglobal.com>
2021-08-31 12:46:46 +0000

    Fully automated

--- Vagrantfile
+++ Vagrantfile
@@ -1,15 +1,15 @@
 Vagrant.configure("2") do |config|    
+    config.vm.define "db" do |db|
+        db.vm.box = "ubuntu/xenial64"
+        db.vm.network "private_network", ip: "192.168.10.150"
+        db.vm.synced_folder "config_files", "/home/vagrant/config_files"
+        db.vm.provision "shell", path: "provision_db.sh"
+    end
     config.vm.define "app" do |app|
         app.vm.box = "ubuntu/xenial64"
         app.vm.network "private_network", ip: "192.168.10.100"
         app.vm.synced_folder "app", "/home/vagrant/app"
         app.vm.synced_folder "config_files", "/home/vagrant/config_files"
         app.vm.provision "shell", path: "provision_app.sh"
-    end
-    config.vm.define "db" do |db|
-        db.vm.box = "ubuntu/xenial64"
-        db.vm.network "private_network", ip: "192.168.10.150"
-        db.vm.synced_folder "config_files", "/home/vagrant/config_files"
-        db.vm.provision "shell", path: "provision_db.sh"
     end
 end
--- provision_app.sh
+++ provision_app.sh
@@ -2,21 +2,18 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
-sudo apt-get install tmux -y
-sudo apt-get install npm -y
-sudo apt-get install python-software-properties
+sudo apt-get install nodejs -y
+sudo apt-get install python-software-properties -y
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 sudo apt-get install nodejs -y
 cd /home/vagrant/app
 sudo npm install pm2 -g
-npm install
+sudo npm install
 sudo rm /etc/nginx/sites-available/default
 sudo ln -s /home/vagrant/config_files/default /etc/nginx/sites-available/default
 sudo nginx -t 
 sudo systemctl restart nginx
-echo "DB_HOST=mongodb://192.168.10.150:27017/posts" | sudo tee -a /etc/environment
-
-# sudo apt-get install git-core zsh -y
-# sudo sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -y)"
-# sudo apt-get install fonts-powerline -y
-
+echo "export DB_HOST=192.168.10.150:27017/posts/" >> /etc/environment
+source /etc/environment
+node seeds/seed.js
+npm start
